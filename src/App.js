9f90e34c79a877849d57778cccb34bf8f4d386fa
megazortti <39544599+megazortti@users.noreply.github.com>
2021-12-19 19:58:09 -0300

    react-update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,103 @@
 import logo from './logo.svg';
 import './App.css';
+import Grid from '@mui/material/Grid';
+import Item from '@mui/material/InputLabel';
+import Skeleton from '@mui/material/Skeleton';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import {Box, TextField, Typography, Link, Avatar} from '@mui/material';
+import Collapse from '@mui/material/Collapse';
+import Todolist from './todo/todo';
+import Axios from 'axios'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Notification from '@bit/ahojukka5.bloglist.notification';
+import { typography } from '@mui/system';
+import Header from './header/header';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const moviesStyle={
+    display: 'flex',
+    flexFlow: 'wrap',
+    marginLeft: '30px '
+  }
+  const movieStyle={
+    marginRight: '10px',
+    marginLeft: '10px',
+    marginBottom: '10px',
+    marginTop: '50px'
+    
+  }
+  return ( 
+    <>
+        <Header/>
+        <Collapse in={1==1}>
+          <div id="movies" style={moviesStyle}>
+            <div id="movie" style={movieStyle}>
+              <Skeleton style={{backgroundColor: 'gray',}} variant="text" width={'400px'} height={'35px'} />
+              <Skeleton style={{backgroundColor: 'gray',}} variant="rectangular" width={'400px'} height={'200px'} />
+            </div>            
+            <div id="movie" style={movieStyle}>
+              <Skeleton style={{backgroundColor: 'gray',}} variant="text" width={'400px'} height={'35px'} />
+              <Skeleton style={{backgroundColor: 'gray',}} variant="rectangular" width={'400px'} height={'200px'} />
+            </div>            
+            <div id="movie" style={movieStyle}>
+              <Skeleton style={{backgroundColor: 'gray',}} variant="text" width={'400px'} height={'35px'} />
+              <Skeleton style={{backgroundColor: 'gray',}} variant="rectangular" width={'400px'} height={'200px'} />
+            </div>            
+            <div id="movie" style={movieStyle}>
+              <Skeleton style={{backgroundColor: 'gray',}} variant="text" width={'400px'} height={'35px'} />
+              <Skeleton style={{backgroundColor: 'gray',}} variant="rectangular" width={'400px'} height={'200px'} />
+            </div>            
+            <div id="movie" style={movieStyle}>
+              <Skeleton style={{backgroundColor: 'gray',}} variant="text" width={'400px'} height={'35px'} />
+              <Skeleton style={{backgroundColor: 'gray',}} variant="rectangular" width={'400px'} height={'200px'} />
+            </div>            
+            <div id="movie" style={movieStyle}>
+              <Skeleton style={{backgroundColor: 'gray',}} variant="text" width={'400px'} height={'35px'} />
+              <Skeleton style={{backgroundColor: 'gray',}} variant="rectangular" width={'400px'} height={'200px'} />
+            </div>            
+            <div id="movie" style={movieStyle}>
+              <Skeleton style={{backgroundColor: 'gray',}} variant="text" width={'400px'} height={'35px'} />
+              <Skeleton style={{backgroundColor: 'gray',}} variant="rectangular" width={'400px'} height={'200px'} />
+            </div>            
+            <div id="movie" style={movieStyle}>
+              <Skeleton style={{backgroundColor: 'gray',}} variant="text" width={'400px'} height={'35px'} />
+              <Skeleton style={{backgroundColor: 'gray',}} variant="rectangular" width={'400px'} height={'200px'} />
+            </div>            
+            <div id="movie" style={movieStyle}>
+              <Skeleton style={{backgroundColor: 'gray',}} variant="text" width={'400px'} height={'35px'} />
+              <Skeleton style={{backgroundColor: 'gray',}} variant="rectangular" width={'400px'} height={'200px'} />
+            </div>            
+            <div id="movie" style={movieStyle}>
+              <Skeleton style={{backgroundColor: 'gray',}} variant="text" width={'400px'} height={'35px'} />
+              <Skeleton style={{backgroundColor: 'gray',}} variant="rectangular" width={'400px'} height={'200px'} />
+            </div>            
+            <div id="movie" style={movieStyle}>
+              <Skeleton style={{backgroundColor: 'gray',}} variant="text" width={'400px'} height={'35px'} />
+              <Skeleton style={{backgroundColor: 'gray',}} variant="rectangular" width={'400px'} height={'200px'} />
+            </div>            
+            <div id="movie" style={movieStyle}>
+              <Skeleton style={{backgroundColor: 'gray',}} variant="text" width={'400px'} height={'35px'} />
+              <Skeleton style={{backgroundColor: 'gray',}} variant="rectangular" width={'400px'} height={'200px'} />
+            </div>            
+            <div id="movie" style={movieStyle}>
+              <Skeleton style={{backgroundColor: 'gray',}} variant="text" width={'400px'} height={'35px'} />
+              <Skeleton style={{backgroundColor: 'gray',}} variant="rectangular" width={'400px'} height={'200px'} />
+            </div>            
+            <div id="movie" style={movieStyle}>
+              <Skeleton style={{backgroundColor: 'gray',}} variant="text" width={'400px'} height={'35px'} />
+              <Skeleton style={{backgroundColor: 'gray',}} variant="rectangular" width={'400px'} height={'200px'} />
+            </div>            
+            <div id="movie" style={movieStyle}>
+              <Skeleton style={{backgroundColor: 'gray',}} variant="text" width={'400px'} height={'35px'} />
+              <Skeleton style={{backgroundColor: 'gray',}} variant="rectangular" width={'400px'} height={'200px'} />
+            </div>            
+          </div>
+        </Collapse>
+    </>
   );
 }
 
